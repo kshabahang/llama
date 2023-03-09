ea88c39b91831ee94c6d5c509472b7ed8dc8e51a
Kevin <k.shabahang@gmail.com>
2023-03-09 17:03:09 -0500

    try again

--- explore.py
+++ explore.py
@@ -20,23 +20,60 @@ def setup_model_parallel() -> Tuple[int, int]:
     local_rank = int(os.environ.get("LOCAL_RANK", -1))
     world_size = int(os.environ.get("WORLD_SIZE", -1))
 
-    #torch.distributed.init_process_group("nccl")
+    torch.distributed.init_process_group("gloo")
     initialize_model_parallel(world_size)
-    torch.cuda.set_device(local_rank)
+    print('Setup parallel complete!')
+    # torch.cuda.set_device(local_rank)
 
     # seed must be the same in all processes
     torch.manual_seed(1)
     return local_rank, world_size
 
 
-#fire.Fire(main)
+def load(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    local_rank: int,
+    world_size: int,
+    max_seq_len: int,
+    max_batch_size: int,
+) -> LLaMA:
+    start_time = time.time()
+    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert world_size == len(
+        checkpoints
+    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+    ckpt_path = checkpoints[local_rank]
+    print("Loading")
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    model_args: ModelArgs = ModelArgs(
+        max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
+    )
+    tokenizer = Tokenizer(model_path=tokenizer_path)
+    model_args.vocab_size = tokenizer.n_words
+    # torch.set_default_tensor_type(torch.cuda.HalfTensor)
+    torch.set_default_tensor_type(torch.BFloat16Tensor)
+    model = Transformer(model_args)
+    torch.set_default_tensor_type(torch.FloatTensor)
+    model.load_state_dict(checkpoint, strict=False)
+
+    generator = LLaMA(model, tokenizer)
+    print(f"Loaded in {time.time() - start_time:.2f} seconds")
+    return generator
+
+
+
+
 
 with open("config.json", "r") as f:
     config = json.loads(f.read())
 
 
 ckpt_dir = config['model_path'] + config['model_size']
-tokenizer_path = config['model_path'] 
+tokenizer_path = config['model_path']
 temperature = 0.8
 top_p =  0.95
 max_seq_len = 512
@@ -50,8 +87,17 @@ generator = load(
     ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
 )
 
-prompts = ["I believe the meaning of life is","Simply put, the theory of relativity states that "]
-results = generator.generate(prompts, max_gen_len=256, temperature=temperature, top_p=top_p )
+prompts = ["I believe the meaning of life is",
+    "Simply put, the theory of relativity states that ",
+    "Building a website can be done in 10 simple steps:\n"]
+
+
+# results = generator.generate(
+#     prompts, max_gen_len=256, temperature=temperature, top_p=top_p
+# )
+results = generator.generate(
+    prompts, max_gen_len=512, temperature=temperature, top_p=top_p
+)
 
 for result in results:
     print(result)
